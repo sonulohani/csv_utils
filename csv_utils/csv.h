9ae805b8639f086fe1e9e3eef78e8a77863288ae
Sonu Lohani <sonulohani@gmail.com>
2019-04-18 19:57:49 +0530

    Add open, close methods; Naming conventions based on the Google C++ style Doesn't work if the data in csv contains spaces; Add support to write the csv files;

--- csv_utils/csv.h
+++ csv_utils/csv.h
@@ -9,90 +9,277 @@
 #include <exception>
 #include <sstream>
 
-namespace {
-	template<const int N>
-	std::array<std::string, N> split_by_delimiter(const std::string& str, char delimiter) {
-		std::array<std::string, N> str_array = {};
-		std::istringstream iss(str);
-		std::string temp;
-		int index = 0;
-		while (std::getline(iss, temp, delimiter) && index < N) {
-			str_array[index++] = temp;
-		}
-		return str_array;
+namespace
+{
+template <const int N>
+std::array<std::string, N> split_by_delimiter(const std::string &str, char delimiter)
+{
+	std::array<std::string, N> splitted_str_array = {};
+	std::istringstream iss(str);
+	std::string temp;
+	int index = 0;
+	while (std::getline(iss, temp, delimiter) && index < N)
+	{
+		splitted_str_array[index++] = temp;
 	}
+	return splitted_str_array;
 }
 
-namespace io {
-	namespace exception {
-		class FileNotOpenedException : public std::exception {
-		public:
-			virtual const char* what() const noexcept override {
-				return "File is not opened";
-			}
-		};
+template <typename T>
+class CastString
+{
+  public:
+	T operator()(const std::string &str) const
+	{
+		std::stringstream stream;
+		stream << str;
+		T castedOutput;
+		stream >> castedOutput;
+		return castedOutput;
+	}
+};
+
+template <>
+class CastString<std::string>
+{
+  public:
+	std::string operator()(const std::string &str) const
+	{
+		return str;
+	}
+};
+} // namespace
+
+namespace io
+{
+namespace exception
+{
+class FileNotOpenedException : public std::exception
+{
+  public:
+	virtual const char *what() const noexcept override
+	{
+		return "File is not opened";
+	}
+};
+
+class FileNotOpenedInReadModeException : std::exception
+{
+  public:
+	virtual const char *what() const noexcept override
+	{
+		return "File is not opened in read mode";
+	}
+};
+class FileNotOpenedInWriteModeException : std::exception
+{
+  public:
+	virtual const char *what() const noexcept override
+	{
+		return "File is not opened in write mode";
+	}
+};
+} // namespace exception
+
+/**
+ * @brief Class to write or read csv files
+ * 
+ * @tparam T 
+ * @tparam Args 
+ */
+template <typename T, typename... Args>
+class Csv
+{
+  public:
+	/**
+	 * @brief Constructs a new Csv object
+	 * 
+	 */
+	explicit Csv() = default;
+
+	/**
+	 * @brief Constructs a new Csv object
+	 * 
+	 * @param file_name Name of the file to be opened for read or write or append
+	 * @param mode Mode in which file to be opened. For mode description head over to:- https://en.cppreference.com/w/cpp/io/ios_base/openmode
+	 * @param file_has_header True if file has header otherwise false
+	 */
+	explicit Csv(std::string &file_name, std::ios_base::openmode mode, bool file_has_header) throw(exception::FileNotOpenedException)
+	{
+		if (!open(file_name, mode, file_has_header))
+		{
+			throw exception::FileNotOpenedException();
+		}
 	}
 
-	template<typename T>
-	class StringToBasicType{
-	public:
-		T operator()(const std::string &str) const {
-			std::stringstream stream;
-			stream << str;
-			T castedOutput;
-			stream >> castedOutput;
-			return castedOutput;
-		}
-	};
+	/**
+	 * @brief Destroys the Csv object
+	 * 
+	 */
+	~Csv()
+	{
+		close();
+	}
 
-	template<typename... Args>
-	class Csv {
-	public:
-		explicit Csv() = default;
+	/**
+	 * @brief Opens the file in mode passed
+	 * 
+	 * @param file_name Name of the file to be opened for read or write or append
+	 * @param mode Mode in which file to be opened. For mode description head over to:- https://en.cppreference.com/w/cpp/io/ios_base/openmode
+	 * @param file_has_header True if file has header otherwise false
+	 * @return true 
+	 * @return false 
+	 */
+	bool open(std::string &file_name, std::ios_base::openmode mode, bool file_has_header) noexcept
+	{
 
-		explicit Csv(std::string& file_name, std::ios_base::openmode mode) throw(exception::FileNotOpenedException) {
-			m_stream.open(file_name.c_str(), mode);
-			if (m_stream.is_open()) {
-				if ((mode & std::ios::in) == std::ios::in) {
-					std::string header_line;
-					std::getline(m_stream, header_line);
-					m_headers = split_by_delimiter<sizeof...(Args)>(header_line, ',');
-				}
+		// close the previous stream if opened
+		close();
+
+		openmode = mode;
+		file_stream.open(file_name.c_str(), openmode);
+
+		if (file_stream.is_open())
+		{
+			if ((openmode & std::ios::in) == std::ios::in && file_has_header)
+			{
+				read_headers_internal();
 			}
-			else {
-				throw exception::FileNotOpenedException();
-			}
+			return true;
 		}
-
-		std::array<std::string, sizeof...(Args)> getHeaders() noexcept {
-			return m_headers;
+		else
+		{
+			return false;
 		}
+	}
 
-		template<typename T, typename... Arguments>
-		void get_values(T &first, Arguments& ... args) {
-			std::string line;
-			std::getline(m_stream, line);
-			get_values_expanded<T, Arguments...>(first, args..., split_by_delimiter<sizeof...(Arguments) + 1>(line, ','));
+	/**
+	 * @brief Returns if file can be read further
+	 * 
+	 * @return true If file can be read further
+	 * @return false If file can't be read further
+	 */
+	bool can_read() throw(exception::FileNotOpenedInReadModeException)
+	{
+		if ((openmode & std::ios::in) != std::ios::in)
+			throw exception::FileNotOpenedInReadModeException();
+
+		return !file_stream.eof();
+	}
+
+	/**
+	 * @brief Closes the file opened
+	 * 
+	 */
+	void close() noexcept
+	{
+		if (file_stream.is_open())
+		{
+			file_stream.close();
 		}
+	}
 
-	private:
-		template<typename T, const int N = 1>
-		void get_values_expanded(T& arg, const std::array<std::string, N>& arr) {
-			StringToBasicType<T> stbt;
-			arg = stbt(arr[N - 1]);
-		}
+	/**
+	 * @brief Returns the csv headers array
+	 * 
+	 * @return auto 
+	 */
+	inline auto get_header() const noexcept
+	{
+		return csv_header;
+	}
 
-		template<typename T, typename... Arguments, const int N = sizeof...(Arguments)>
-		void get_values_expanded(T& first, Arguments& ... args, const std::array<std::string, N>& arr) {
-			StringToBasicType<T> stbt;
-			first = stbt(arr[N - 2]);
-			get_values_expanded(args..., arr);
-		}
+	/**
+	 * @brief Reads comma seperated values from the file
+	 * 
+	 * @param first 
+	 * @param args 
+	 */
+	void read_values(T &first, Args &... args) throw(exception::FileNotOpenedInReadModeException)
+	{
+		if ((openmode & std::ios::in) != std::ios::in)
+			throw exception::FileNotOpenedInReadModeException();
 
-	private:
-		std::fstream m_stream;
-		std::array<std::string, sizeof...(Args)> m_headers;
-	};
-}
+		std::string line;
+		std::getline(file_stream, line);
+		auto values = split_by_delimiter<sizeof...(Args) + 1>(line, ',');
+		first = values[0];
+		read_values_expanded<sizeof...(Args) + 1, Args...>(args..., values);
+	}
+
+	/**
+	 * @brief Sets the csv header to the file. Should be called at the start of writing of the file
+	 * 
+	 * @tparam T 
+	 * @tparam Args 
+	 * @param first 
+	 * @param args 
+	 */
+	template <typename T, typename... Args>
+	void set_header(const T &first, const Args &... args) throw(exception::FileNotOpenedInWriteModeException)
+	{
+		if ((openmode & std::ios::out) != std::ios::out)
+			throw exception::FileNotOpenedInWriteModeException();
+
+		file_stream << first << ',';
+		write_header(args...);
+	}
+
+	/**
+	 * @brief Writes command seperated values to the file.
+	 * 
+	 * @param first 
+	 * @param args 
+	 */
+	void write_values(const T &first, const Args &... args) throw(exception::FileNotOpenedInWriteModeException)
+	{
+		if ((openmode & std::ios::out) != std::ios::out)
+			throw exception::FileNotOpenedInWriteModeException();
+
+		file_stream << first << ',';
+		write_values(args...);
+	}
+
+  private:
+	void read_headers_internal()
+	{
+		std::string header_line;
+		std::getline(file_stream, header_line);
+		csv_header = split_by_delimiter<sizeof...(Args) + 1>(header_line, ',');
+	}
+
+	template <const int N, typename T>
+	void read_values_expanded(T &arg, const std::array<std::string, N> &arr)
+	{
+		CastString<T> cs;
+		arg = cs(arr[N - 1]);
+	}
+
+	template <const int N, typename T, typename... Args>
+	void read_values_expanded(T &first, Args &... args, const std::array<std::string, N> &arr)
+	{
+		CastString<T> cs;
+		first = cs(arr[N - sizeof...(Args) - 1]);
+		read_values_expanded<N, Args...>(args..., arr);
+	}
+
+	template <typename T>
+	void write_values(const T &value)
+	{
+		file_stream << value << '\n';
+	}
+
+	template <typename T>
+	void write_header(const T &value)
+	{
+		write_values(value);
+	}
+
+  private:
+	std::fstream file_stream;
+	std::array<std::string, sizeof...(Args) + 1> csv_header;
+	std::ios_base::openmode openmode;
+};
+} // namespace io
 
 #endif // !CSV_H
