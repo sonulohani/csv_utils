57c54e6ea7d5d7747b430499eccb1749240e4d38
sonulohani <sonulohani@gmail.com>
2019-12-08 20:21:51 +0530

    Apply clang-format

--- include/csv.h
+++ include/csv.h
@@ -1,52 +1,49 @@
 #ifndef CSV_H
 #define CSV_H
 
-#include <iostream>
-#include <fstream>
 #include <array>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <exception>
-#include <sstream>
 
 namespace
 {
-template <const int N>
-std::array<std::string, N> split_by_delimiter(const std::string &str, char delimiter)
+template<const int N> std::array<std::string, N> split_by_delimiter(const std::string &str, char delimiter)
 {
-	std::array<std::string, N> splitted_str_array = {};
-	std::istringstream iss(str);
-	std::string temp;
-	int index = 0;
-	while (std::getline(iss, temp, delimiter) && index < N)
-	{
-		splitted_str_array[index++] = temp;
-	}
-	return splitted_str_array;
+    std::array<std::string, N> splitted_str_array = {};
+    std::istringstream iss(str);
+    std::string temp;
+    int index = 0;
+    while (std::getline(iss, temp, delimiter) && index < N)
+    {
+        splitted_str_array[index++] = temp;
+    }
+    return splitted_str_array;
 }
 
-template <typename T>
-class CastString
+template<typename T> class CastString
 {
   public:
-	T operator()(const std::string &str) const
-	{
-		std::stringstream stream;
-		stream << str;
-		T castedOutput;
-		stream >> castedOutput;
-		return castedOutput;
-	}
+    T operator()(const std::string &str) const
+    {
+        std::stringstream stream;
+        stream << str;
+        T castedOutput;
+        stream >> castedOutput;
+        return castedOutput;
+    }
 };
 
-template <>
-class CastString<std::string>
+template<> class CastString<std::string>
 {
   public:
-	std::string operator()(const std::string &str) const
-	{
-		return str;
-	}
+    std::string operator()(const std::string &str) const
+    {
+        return str;
+    }
 };
 } // namespace
 
@@ -57,249 +54,248 @@ namespace exception
 class FileNotOpenedException : public std::exception
 {
   public:
-	virtual const char *what() const noexcept override
-	{
-		return "File is not opened";
-	}
+    virtual const char *what() const noexcept override
+    {
+        return "File is not opened";
+    }
 };
 
 class FileNotOpenedInReadModeException : std::exception
 {
   public:
-	virtual const char *what() const noexcept override
-	{
-		return "File is not opened in read mode";
-	}
+    virtual const char *what() const noexcept override
+    {
+        return "File is not opened in read mode";
+    }
 };
 class FileNotOpenedInWriteModeException : std::exception
 {
   public:
-	virtual const char *what() const noexcept override
-	{
-		return "File is not opened in write mode";
-	}
+    virtual const char *what() const noexcept override
+    {
+        return "File is not opened in write mode";
+    }
 };
 } // namespace exception
 
 /**
  * @brief Class to write or read csv files
- * 
- * @tparam T 
- * @tparam Args 
+ *
+ * @tparam T
+ * @tparam Args
  */
-template<typename T, typename... Args>
-class Csv
+template<typename T, typename... Args> class Csv
 {
   public:
-	/**
-	 * @brief Constructs a new Csv object
-	 * 
-	 */
-	explicit Csv() = default;
+    /**
+     * @brief Constructs a new Csv object
+     *
+     */
+    explicit Csv() = default;
 
-	/**
-	 * @brief Constructs a new Csv object
-	 * 
-	 * @param file_name Name of the file to be opened for read or write or append
-	 * @param mode Mode in which file to be opened. For mode description head over to:- https://en.cppreference.com/w/cpp/io/ios_base/openmode
-	 * @param file_has_header True if file has header otherwise false
-	 */
-	explicit Csv(const std::string &file_name, std::ios_base::openmode mode, bool file_has_header)
-	{
-		open(file_name, mode, file_has_header);
-	}
+    /**
+     * @brief Constructs a new Csv object
+     *
+     * @param file_name Name of the file to be opened for read or write or append
+     * @param mode Mode in which file to be opened. For mode description head over
+     * to:- https://en.cppreference.com/w/cpp/io/ios_base/openmode
+     * @param file_has_header True if file has header otherwise false
+     */
+    explicit Csv(const std::string &file_name, std::ios_base::openmode mode, bool file_has_header)
+    {
+        open(file_name, mode, file_has_header);
+    }
 
-	/**
-	 * @brief Destroys the Csv object
-	 * 
-	 */
-	~Csv()
-	{
-		close();
-	}
+    /**
+     * @brief Destroys the Csv object
+     *
+     */
+    ~Csv()
+    {
+        close();
+    }
 
-	inline bool is_open() const
-	{
-		return file_stream.is_open();
-	}
+    inline bool is_open() const
+    {
+        return file_stream.is_open();
+    }
 
-	/**
-	 * @brief Opens the file in mode passed
-	 * 
-	 * @param file_name Name of the file to be opened for read or write or append
-	 * @param mode Mode in which file to be opened. For mode description head over to:- https://en.cppreference.com/w/cpp/io/ios_base/openmode
-	 * @param file_has_header True if file has header otherwise false
-	 * @return true 
-	 * @return false 
-	 */
-	bool open(const std::string &file_name, std::ios_base::openmode mode, bool file_has_header) noexcept
-	{
+    /**
+     * @brief Opens the file in mode passed
+     *
+     * @param file_name Name of the file to be opened for read or write or append
+     * @param mode Mode in which file to be opened. For mode description head over
+     * to:- https://en.cppreference.com/w/cpp/io/ios_base/openmode
+     * @param file_has_header True if file has header otherwise false
+     * @return true
+     * @return false
+     */
+    bool open(const std::string &file_name, std::ios_base::openmode mode, bool file_has_header) noexcept
+    {
 
-		// close the previous stream if opened
-		close();
+        // close the previous stream if opened
+        close();
 
-		openmode = mode;
-		file_stream.open(file_name.c_str(), openmode);
+        openmode = mode;
+        file_stream.open(file_name.c_str(), openmode);
 
-		if (file_stream.is_open())
-		{
-			if ((openmode & std::ios::in) == std::ios::in && file_has_header)
-			{
-				read_headers_internal();
-			}
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+        if (file_stream.is_open())
+        {
+            if ((openmode & std::ios::in) == std::ios::in && file_has_header)
+            {
+                read_headers_internal();
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-	/**
-	 * @brief Returns if file can be read further
-	 * 
-	 * @return true If file can be read further
-	 * @return false If file can't be read further
-	 */
-	bool can_read() const
-	{
-		if ((openmode & std::ios::in) != std::ios::in)
-			throw exception::FileNotOpenedInReadModeException();
+    /**
+     * @brief Returns if file can be read further
+     *
+     * @return true If file can be read further
+     * @return false If file can't be read further
+     */
+    bool can_read() const
+    {
+        if ((openmode & std::ios::in) != std::ios::in)
+            throw exception::FileNotOpenedInReadModeException();
 
-		return !file_stream.eof();
-	}
+        return !file_stream.eof();
+    }
 
-	/**
-	 * @brief Closes the file opened
-	 * 
-	 */
-	void close() noexcept
-	{
-		if (file_stream.is_open())
-		{
-			file_stream.close();
-		}
-	}
+    /**
+     * @brief Closes the file opened
+     *
+     */
+    void close() noexcept
+    {
+        if (file_stream.is_open())
+        {
+            file_stream.close();
+        }
+    }
 
-	/**
-	 * @brief Returns the csv headers array
-	 * 
-	 * @return auto 
-	 */
-	inline std::array<std::string, sizeof...(Args) + 1> get_header() const noexcept
-	{
-		if(!file_stream.is_open())
-			throw exception::FileNotOpenedException();
+    /**
+     * @brief Returns the csv headers array
+     *
+     * @return auto
+     */
+    inline std::array<std::string, sizeof...(Args) + 1> get_header() const noexcept
+    {
+        if (!file_stream.is_open())
+            throw exception::FileNotOpenedException();
 
-		if ((openmode & std::ios::in)!= std::ios::in)
-			throw exception::FileNotOpenedInReadModeException();
+        if ((openmode & std::ios::in) != std::ios::in)
+            throw exception::FileNotOpenedInReadModeException();
 
-			return csv_header;
-	}
+        return csv_header;
+    }
 
-	/**
-	 * @brief Reads comma seperated values from the file
-	 * 
-	 * @param first 
-	 * @param args 
-	 */
-	void read_values(T &first, Args &... args)
-	{
-		if ((openmode & std::ios::in) != std::ios::in)
-			throw exception::FileNotOpenedInReadModeException();
+    /**
+     * @brief Reads comma seperated values from the file
+     *
+     * @param first
+     * @param args
+     */
+    void read_values(T &first, Args &... args)
+    {
+        if ((openmode & std::ios::in) != std::ios::in)
+            throw exception::FileNotOpenedInReadModeException();
 
-		std::string line;
-		std::getline(file_stream, line);
-		auto values = split_by_delimiter<sizeof...(Args) + 1>(line, ',');
-		first = values[0];
-		if constexpr (sizeof...(Args))
-			read_values_internal<sizeof...(Args) + 1, Args...>(args..., values);
-	}
+        std::string line;
+        std::getline(file_stream, line);
+        auto values = split_by_delimiter<sizeof...(Args) + 1>(line, ',');
+        first = values[0];
+        if constexpr (sizeof...(Args))
+            read_values_internal<sizeof...(Args) + 1, Args...>(args..., values);
+    }
 
-	/**
-	 * @brief Sets the csv header to the file. Should be called at the start of writing of the file
-	 * 
-	 * @tparam T 
-	 * @tparam Args 
-	 * @param first 
-	 * @param args 
-	 */
-	template <typename Type, typename... Arguments>
-	void set_header(const Type &first, const Arguments &... args)
-	{
-		if ((openmode & std::ios::out) != std::ios::out)
-			throw exception::FileNotOpenedInWriteModeException();
+    /**
+     * @brief Sets the csv header to the file. Should be called at the start of
+     * writing of the file
+     *
+     * @tparam T
+     * @tparam Args
+     * @param first
+     * @param args
+     */
+    template<typename Type, typename... Arguments> void set_header(const Type &first, const Arguments &... args)
+    {
+        if ((openmode & std::ios::out) != std::ios::out)
+            throw exception::FileNotOpenedInWriteModeException();
 
-		file_stream << first << ',';
-		write_values(args...);
-	}
+        file_stream << first << ',';
+        write_values(args...);
+    }
 
-	/**
-	 * @brief Sets the csv header to the file. Should be called at the start of writing of the file
-	 * 
-	 * @tparam T 
-	 * @param value 
-	 */
-	template <typename Type>
-	void set_header(const Type &value)
-	{
-		write_values(value);
-	}
+    /**
+     * @brief Sets the csv header to the file. Should be called at the start of
+     * writing of the file
+     *
+     * @tparam T
+     * @param value
+     */
+    template<typename Type> void set_header(const Type &value)
+    {
+        write_values(value);
+    }
 
-	/**
-	 * @brief Writes command seperated values to the file.
-	 * 
-	 * @param first 
-	 * @param args 
-	 */
-	void write_values(const T &first, const Args &... args)
-	{
-		if ((openmode & std::ios::out) != std::ios::out)
-			throw exception::FileNotOpenedInWriteModeException();
+    /**
+     * @brief Writes command seperated values to the file.
+     *
+     * @param first
+     * @param args
+     */
+    void write_values(const T &first, const Args &... args)
+    {
+        if ((openmode & std::ios::out) != std::ios::out)
+            throw exception::FileNotOpenedInWriteModeException();
 
-		file_stream << first << ',';
-		write_values(args...);
-	}
+        file_stream << first << ',';
+        write_values(args...);
+    }
 
-	/**
-	 * @brief Writes command seperated values to the file.
-	 * 
-	 * @tparam T 
-	 * @param value 
-	 */
-	template <typename Type>
-	void write_values(const Type &value)
-	{
-		file_stream << value << '\n';
-	}
-
-  private:
-	void read_headers_internal()
-	{
-		std::string header_line;
-		std::getline(file_stream, header_line);
-		csv_header = split_by_delimiter<sizeof...(Args) + 1>(header_line, ',');
-	}
-
-	template <const int N, typename Type>
-	void read_values_internal(Type &arg, const std::array<std::string, N> &arr)
-	{
-		CastString<Type> cs;
-		arg = cs(arr[N - 1]);
-	}
-
-	template <const int N, typename Type, typename... Arguments>
-	void read_values_internal(Type &first, Arguments &... args, const std::array<std::string, N> &arr)
-	{
-		CastString<Type> cs;
-		first = cs(arr[N - sizeof...(Arguments) - 1]);
-		read_values_internal<N, Arguments...>(args..., arr);
-	}
+    /**
+     * @brief Writes command seperated values to the file.
+     *
+     * @tparam T
+     * @param value
+     */
+    template<typename Type> void write_values(const Type &value)
+    {
+        file_stream << value << '\n';
+    }
 
   private:
-	std::fstream file_stream;
-	std::array<std::string, sizeof...(Args) + 1> csv_header;
-	std::ios_base::openmode openmode;
+    void read_headers_internal()
+    {
+        std::string header_line;
+        std::getline(file_stream, header_line);
+        csv_header = split_by_delimiter<sizeof...(Args) + 1>(header_line, ',');
+    }
+
+    template<const int N, typename Type> void read_values_internal(Type &arg, const std::array<std::string, N> &arr)
+    {
+        CastString<Type> cs;
+        arg = cs(arr[N - 1]);
+    }
+
+    template<const int N, typename Type, typename... Arguments>
+    void read_values_internal(Type &first, Arguments &... args, const std::array<std::string, N> &arr)
+    {
+        CastString<Type> cs;
+        first = cs(arr[N - sizeof...(Arguments) - 1]);
+        read_values_internal<N, Arguments...>(args..., arr);
+    }
+
+  private:
+    std::fstream file_stream;
+    std::array<std::string, sizeof...(Args) + 1> csv_header;
+    std::ios_base::openmode openmode;
 };
 } // namespace io
 
--- unit-tests/csv_read_test.cpp
+++ unit-tests/csv_read_test.cpp
@@ -5,94 +5,88 @@
 
 TEST(CsvRead, TestOpenInCtor)
 {
-	// Opening file which is not in existence
-	io::Csv<std::string> csv("abc.csv", std::ios_base::in, true);
-	EXPECT_FALSE(csv.is_open());
+    // Opening file which is not in existence
+    io::Csv<std::string> csv("abc.csv", std::ios_base::in, true);
+    EXPECT_FALSE(csv.is_open());
 
-	io::Csv<std::string> csv1(std::string("100_Sales_Records.csv"),
-		std::ios_base::in, true);
-	EXPECT_TRUE(csv1.is_open());
+    io::Csv<std::string> csv1(std::string("100_Sales_Records.csv"), std::ios_base::in, true);
+    EXPECT_TRUE(csv1.is_open());
 }
 
 TEST(CsvRead, TestOpen)
 {
-	// Opening file which is not in existence
-	io::Csv<std::string> csv;
-	EXPECT_FALSE(csv.open("abc.csv", std::ios_base::in, true));
-	EXPECT_FALSE(csv.is_open());
+    // Opening file which is not in existence
+    io::Csv<std::string> csv;
+    EXPECT_FALSE(csv.open("abc.csv", std::ios_base::in, true));
+    EXPECT_FALSE(csv.is_open());
 
-	EXPECT_TRUE(csv.open(std::string("100_Sales_Records.csv"),
-		std::ios_base::in, true));
-	EXPECT_TRUE(csv.is_open());
+    EXPECT_TRUE(csv.open(std::string("100_Sales_Records.csv"), std::ios_base::in, true));
+    EXPECT_TRUE(csv.is_open());
 }
 
 TEST(CsvRead, TestHeader)
 {
-	io::Csv<std::string> csv1(std::string("100_Sales_Records.csv"),
-					std::ios_base::in, true);
-	auto header1 = csv1.get_header();
-	ASSERT_EQ(1, static_cast<int>(header1.size()));
-	EXPECT_STREQ("Region", header1[0].c_str());
+    io::Csv<std::string> csv1(std::string("100_Sales_Records.csv"), std::ios_base::in, true);
+    auto header1 = csv1.get_header();
+    ASSERT_EQ(1, static_cast<int>(header1.size()));
+    EXPECT_STREQ("Region", header1[0].c_str());
 
-	io::Csv<std::string, std::string> csv2(std::string("100_Sales_Records.csv"),
-		std::ios_base::in, true);
-	auto header2 = csv2.get_header();
-	ASSERT_EQ(2, static_cast<int>(header2.size()));
-	EXPECT_STREQ("Region", header2[0].c_str());
-	EXPECT_STREQ("Country", header2[1].c_str());
+    io::Csv<std::string, std::string> csv2(std::string("100_Sales_Records.csv"), std::ios_base::in, true);
+    auto header2 = csv2.get_header();
+    ASSERT_EQ(2, static_cast<int>(header2.size()));
+    EXPECT_STREQ("Region", header2[0].c_str());
+    EXPECT_STREQ("Country", header2[1].c_str());
 
-	io::Csv<std::string, std::string, std::string, std::string, std::string> csv3(std::string("100_Sales_Records.csv"),
-		std::ios_base::in, true);
-	auto header3 = csv3.get_header();
-	ASSERT_EQ(5, static_cast<int>(header3.size()));
-	EXPECT_STREQ("Region", header3[0].c_str());
-	EXPECT_STREQ("Country", header3[1].c_str());
-	EXPECT_STREQ("Item Type", header3[2].c_str());
-	EXPECT_STREQ("Sales Channel", header3[3].c_str());
-	EXPECT_STREQ("Order Priority", header3[4].c_str());
+    io::Csv<std::string, std::string, std::string, std::string, std::string> csv3(std::string("100_Sales_Records.csv"),
+                                                                                  std::ios_base::in, true);
+    auto header3 = csv3.get_header();
+    ASSERT_EQ(5, static_cast<int>(header3.size()));
+    EXPECT_STREQ("Region", header3[0].c_str());
+    EXPECT_STREQ("Country", header3[1].c_str());
+    EXPECT_STREQ("Item Type", header3[2].c_str());
+    EXPECT_STREQ("Sales Channel", header3[3].c_str());
+    EXPECT_STREQ("Order Priority", header3[4].c_str());
 }
 
 TEST(CsvRead, TestHeaderBeyondRange)
 {
-	io::Csv<std::string, std::string, std::string, std::string, std::string, std::string,
-		std::string, std::string, std::string, std::string, std::string, std::string, std::string,
-		std::string, std::string, std::string, std::string> csv(std::string("100_Sales_Records.csv"),
-			std::ios_base::in, true);
+    io::Csv<std::string, std::string, std::string, std::string, std::string, std::string, std::string, std::string,
+            std::string, std::string, std::string, std::string, std::string, std::string, std::string, std::string,
+            std::string>
+        csv(std::string("100_Sales_Records.csv"), std::ios_base::in, true);
 
-	auto header = csv.get_header();
-	ASSERT_EQ(17, static_cast<int>(header.size()));
-	EXPECT_STREQ("Total Cost", header[12].c_str());
-	EXPECT_STREQ("Total Profit", header[13].c_str());
-	EXPECT_TRUE(header[14].empty());
-	EXPECT_TRUE(header[15].empty());
-	EXPECT_TRUE(header[16].empty());
+    auto header = csv.get_header();
+    ASSERT_EQ(17, static_cast<int>(header.size()));
+    EXPECT_STREQ("Total Cost", header[12].c_str());
+    EXPECT_STREQ("Total Profit", header[13].c_str());
+    EXPECT_TRUE(header[14].empty());
+    EXPECT_TRUE(header[15].empty());
+    EXPECT_TRUE(header[16].empty());
 }
 
 TEST(CsvRead, TestSingleValue)
 {
-	io::Csv<std::string> csv(std::string("100_Sales_Records.csv"),
-		std::ios_base::in, true);
-	std::string region;
-	csv.read_values(region);
-	EXPECT_STREQ("Australia and Oceania", region.c_str());
+    io::Csv<std::string> csv(std::string("100_Sales_Records.csv"), std::ios_base::in, true);
+    std::string region;
+    csv.read_values(region);
+    EXPECT_STREQ("Australia and Oceania", region.c_str());
 }
 
 TEST(CsvRead, TestMultipleValue)
 {
-	io::Csv<std::string, std::string, std::string> csv(std::string("100_Sales_Records.csv"),
-		std::ios_base::in, true);
-	std::string region, country, item_type;
-	csv.read_values(region, country, item_type);
-	EXPECT_STREQ("Australia and Oceania", region.c_str());
-	EXPECT_STREQ("Tuvalu", country.c_str());
-	EXPECT_STREQ("Baby Food", item_type.c_str());
-	csv.read_values(region, country, item_type);
-	EXPECT_STREQ("Central America and the Caribbean", region.c_str());
-	EXPECT_STREQ("Grenada", country.c_str());
-	EXPECT_STREQ("Cereal", item_type.c_str());
+    io::Csv<std::string, std::string, std::string> csv(std::string("100_Sales_Records.csv"), std::ios_base::in, true);
+    std::string region, country, item_type;
+    csv.read_values(region, country, item_type);
+    EXPECT_STREQ("Australia and Oceania", region.c_str());
+    EXPECT_STREQ("Tuvalu", country.c_str());
+    EXPECT_STREQ("Baby Food", item_type.c_str());
+    csv.read_values(region, country, item_type);
+    EXPECT_STREQ("Central America and the Caribbean", region.c_str());
+    EXPECT_STREQ("Grenada", country.c_str());
+    EXPECT_STREQ("Cereal", item_type.c_str());
 }
 
-//TEST(CsvRead, TestHeaderWhenFileNameIsInvalid)
+// TEST(CsvRead, TestHeaderWhenFileNameIsInvalid)
 //{
 //	io::Csv<std::string> csv(std::string("abc.csv"),
 //			std::ios_base::in, true);
